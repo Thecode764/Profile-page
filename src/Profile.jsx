d6214098893ed7c711e8875c9c408d2955169700
Artin karimi <141347375+Thecode764@users.noreply.github.com>
2024-06-03 08:51:42 +0330

    Update Profile.jsx

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,13 @@
 import "./output.css"
 import Skills from "./Skills.jsx"
-import "user.png"
+import user from "./user.png"
 
 export default function Profile() {
     return (
         <>
         <div>
             <div className="flex justify-center items-center">
-            <img src="user.png" width="100" />
+            <img src="{{user}} width="100" />
             </div>
             <h3 className="text-center font-mono mt-5 text-2xl text-white">Artin karimi</h3>
             <p className="text-center text-white font-mono">A web developer 🌐 I code python too 🐍 Here is profile page project! This is a test page created by profile page 🧪<br />🐈 <a href='https://github.com/Thecode764' className="underline">Github</a></p>
